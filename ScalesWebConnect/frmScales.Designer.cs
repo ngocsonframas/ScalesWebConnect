1efda6e67ed2eafdeaecf249375ab79148e06c80
ngocsonframas <nguyenngocson9556@gmail.com>
2020-02-21 15:04:46 +0700

    Fist commit

---
+++ ScalesWebConnect/frmScales.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ScalesWebConnect
+{
+    partial class frmScales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPortCom = new System.Windows.Forms.ComboBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbPortCom
+            // 
+            this.cbPortCom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.cbPortCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPortCom.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPortCom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.cbPortCom.FormattingEnabled = true;
+            this.cbPortCom.Location = new System.Drawing.Point(12, 12);
+            this.cbPortCom.Name = "cbPortCom";
+            this.cbPortCom.Size = new System.Drawing.Size(203, 39);
+            this.cbPortCom.TabIndex = 0;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtWeight.BackColor = System.Drawing.Color.Aqua;
+            this.txtWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWeight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.txtWeight.Location = new System.Drawing.Point(12, 56);
+            this.txtWeight.Multiline = true;
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.ReadOnly = true;
+            this.txtWeight.Size = new System.Drawing.Size(776, 275);
+            this.txtWeight.TabIndex = 1;
+            this.txtWeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmScales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 343);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.cbPortCom);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmScales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SCALES";
+            this.Load += new System.EventHandler(this.frmScales_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPortCom;
+        private System.Windows.Forms.TextBox txtWeight;
+    }
+}
+
---
+++ ScalesWebConnect/frmScales.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace ScalesWebConnect
+{
+    public partial class frmScales : Form
+    {
+        private SerialPort _serialPort;         //<-- declares a SerialPort Variable to be used throughout the form
+        private const int BaudRate = 9600;      //<-- BaudRate Constant. 9600 seems to be the scale-units default value
+
+        public frmScales()
+        {
+            InitializeComponent();
+        }
+
+        private void frmScales_Load(object sender, EventArgs e)
+        {
+            string[] portNames = SerialPort.GetPortNames();     //<-- Reads all available comPorts
+            foreach (var portName in portNames)
+            {
+                cbPortCom.Items.Add(portName);                  //<-- Adds Ports to combobox
+            }
+            cbPortCom.SelectedIndex = 0;                        //<-- Selects first entry (convenience purposes)
+
+            //<-- This block ensures that no exceptions happen
+            if (_serialPort != null && _serialPort.IsOpen)
+                _serialPort.Close();
+            if (_serialPort != null)
+                _serialPort.Dispose();
+            //<-- End of Block
+
+            _serialPort = new SerialPort(cbPortCom.Text, BaudRate, Parity.None, 8, StopBits.One);       //<-- Creates new SerialPort using the name selected in the combobox
+            _serialPort.DataReceived += SerialPortOnDataReceived;       //<-- this event happens everytime when new data is received by the ComPort
+            _serialPort.Open();     //<-- make the comport listen
+            txtWeight.Text = "Listening on " + _serialPort.PortName + "...\r\n";
+
+        }
+
+        private delegate void Closure();
+        private void SerialPortOnDataReceived(object sender, SerialDataReceivedEventArgs serialDataReceivedEventArgs)
+        {
+            if (InvokeRequired)     //<-- Makes sure the function is invoked to work properly in the UI-Thread
+                BeginInvoke(new Closure(() => { SerialPortOnDataReceived(sender, serialDataReceivedEventArgs); }));     //<-- Function invokes itself
+            else
+            {
+                int dataLength = _serialPort.BytesToRead;
+
+                byte[] data = new byte[dataLength];
+                int nbrDataRead = _serialPort.Read(data, 0, dataLength);
+                if (nbrDataRead == 0)
+                    return;
+                string str = Encoding.UTF8.GetString(data);
+
+                //Buffers values in a file
+                File.AppendAllText("buffer1", str);
+
+                //Read from buffer and write into "strnew" String
+                string strnew = File.ReadLines("buffer1").Last();
+
+                //Shows actual true value coming from scale
+                txtWeight.Text = strnew;
+                Regex digits = new Regex(@"^\D*?((-?(\d+(\.\d+)?))|(-?\.\d+)).*");
+                Match mx = digits.Match(txtWeight.Text);
+                decimal strValue1 = mx.Success ? Convert.ToDecimal(mx.Groups[1].Value) : 0;
+                txtWeight.Text = strValue1.ToString();
+
+                //Connect and send data for web app 
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:57760/Mixing/Index");
+                request.Method = "POST";
+                byte[] byteArray = Encoding.UTF8.GetBytes(strValue1.ToString());
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+                WebResponse response = request.GetResponse();
+                dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = HttpUtility.UrlDecode(reader.ReadToEnd());
+                //You may need HttpUtility.HtmlDecode depending on the response
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+        }
+    }
+}
